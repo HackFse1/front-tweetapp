e0568142d20bc464768bb28f959da82925e1b4b2
unknown <cogjava2403@iiht.tech>
2022-09-26 16:09:52 +0530

    first commit

---
+++ tweetappui/src/setupProxy.js
@@ -0,0 +1,31 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+ 
+module.exports = function (app) {
+  app.use(
+    createProxyMiddleware('/api1', {
+      target: 'http://userservicetweet-env.eba-t3mc3qn3.ap-northeast-1.elasticbeanstalk.com/', // API endpoint 1
+      //target: 'http://3.87.108.193:8082',
+      changeOrigin: true,
+      pathRewrite: {
+        "^/api1": "",
+      },
+      headers: {
+        Connection: "keep-alive"
+      }
+    })
+  );
+  app.use(
+    createProxyMiddleware('/api2', {
+      //target: 'http://localhost:8083', // API endpoint 2
+      target: 'http://tweetservicetweet-env.eba-xxgi2phv.ap-northeast-1.elasticbeanstalk.com/',
+      //target: 'http://34.229.167.216:8083',
+      changeOrigin: true,
+      pathRewrite: {
+        "^/api2": "",
+      },
+      headers: {
+        Connection: "keep-alive"
+      }
+    })
+  );
+}
