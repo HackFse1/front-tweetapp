05b4ff1bbb23b24725f8067968ea0f45e6bdd689
HackFse1 <113370032+HackFse1@users.noreply.github.com>
2022-09-27 11:44:33 +0530

    Update setupProxy.js

--- tweetappui/src/setupProxy.js
+++ tweetappui/src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     createProxyMiddleware('/api1', {
-      target: 'http://userservicetweet-env.eba-t3mc3qn3.ap-northeast-1.elasticbeanstalk.com/', // API endpoint 1
+      target: 'http://recordinguserservice-env.eba-mrb2nnmb.ap-northeast-1.elasticbeanstalk.com/', // API endpoint 1
       //target: 'http://3.87.108.193:8082',
       changeOrigin: true,
       pathRewrite: {
@@ -17,7 +17,7 @@ module.exports = function (app) {
   app.use(
     createProxyMiddleware('/api2', {
       //target: 'http://localhost:8083', // API endpoint 2
-      target: 'http://tweetservicetweet-env.eba-xxgi2phv.ap-northeast-1.elasticbeanstalk.com/',
+      target: 'http://recordingtweetservice-env.eba-39dxbbwp.ap-northeast-1.elasticbeanstalk.com/',
       //target: 'http://34.229.167.216:8083',
       changeOrigin: true,
       pathRewrite: {
